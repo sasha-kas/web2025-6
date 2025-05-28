f0fe43e76ea98e772f6a2592ec80ed342d879181
Sasha Kasyanyk web2025-6 <kasyanyksasha@gmail.com>
2025-05-28 17:39:07 +0300

    додано всі методи окрім останніх двох, виюачте я забула комітити

--- index.js
+++ index.js
@@ -32,6 +32,54 @@ app.get('/notes/:name', async (req, res) => {
     res.status(404).send('Note not found');
   }
 });
+app.use(express.text());
+app.put('/notes/:name', async (req, res) => {
+  const noteName = req.params.name;
+  const filePath = `${cache}/${noteName}`;
+
+  try {
+    await fs.promises.access(filePath);
+    await fs.promises.writeFile(filePath, req.body, 'utf-8');
+    res.status(200).send('Note updated');
+  } catch (err) {
+    res.status(404).send('Note not found');
+  }
+});
+app.delete('/notes/:name', async (req, res) => {
+  const noteName = req.params.name;
+  const filePath = `${cache}/${noteName}`;
+
+  try {
+    await fs.promises.access(filePath); 
+    await fs.promises.unlink(filePath); //видалення
+    res.status(200).send('Note deleted');
+  }
+    catch (err) {
+    res.status(404).send('Note not found');
+  }
+});
+app.get('/notes', async (req, res) => {
+  try {
+    const files = await fs.promises.readdir(cache);
+    const notes = [];
+
+    for (const file of files) {
+      const filePath = `${cache}/${file}`;
+      const stat = await fs.promises.stat(filePath);
+
+      if (!stat.isFile() || file.startsWith('.') || file === '.DS_Store') continue; 
+        const text = await fs.promises.readFile(filePath, 'utf-8');
+        notes.push({ name: file, text });
+    }
+    if (notes.length === 0) {
+      res.status(404).send('No notes found');
+    } else {
+    res.status(200).json(notes);
+  }
+   } catch (err) {
+    res.status(500).send('Failed to read notes');
+  }
+});
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);

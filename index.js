4acb3a68aa2fb242c5c496b65e60ed8e9afa92a0
Sasha Kasyanyk web2025-6 <kasyanyksasha@gmail.com>
2025-05-28 15:56:57 +0300

    метод get виправлено всьо працює

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { program } = require('commander');
 const fs = require('fs');
+const fsp = fs.promises;
+const path = require('path');
 
 program
   .requiredOption('-h, --host <host>', 'Host')
@@ -18,6 +20,18 @@ if (!fs.existsSync(cache)) {
 const app = express();
 
 app.get('/', (req, res) => res.send('Server is running.'));
+app.get('/notes/:name', async (req, res) => {
+  const noteName = req.params.name;
+  const filePath = `${cache}/${noteName}`;
+
+  try {
+    await fs.promises.access(filePath);
+    const content = await fs.promises.readFile(filePath, 'utf-8');
+    res.status(200).send(content);
+  } catch (err) {
+    res.status(404).send('Note not found');
+  }
+});
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
